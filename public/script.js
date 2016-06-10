8fc22a534a919f367f776c52abf0feaf2fc5b3ab
wakelank <wakelank@gmail.com>
2016-06-10 17:41:27 -0400

    Gets response from omdb

--- public/script.js
+++ public/script.js
@@ -1,8 +1,21 @@
 window.onload = function(){
   document.getElementById('movieSearchFormSubmit').onclick = function(e){
     e.preventDefault();
-    console.log('click');
-    var searchValue = document.getElementById('movieSearch').searchParam.value;
-    console.log(searchValue);
+    var searchValue = document.getElementById('movieSearchForm').searchParam.value;
+    
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+        console.log(JSON.parse(request.response));
+      }
+    };
+    var url = "http://www.omdbapi.com/?s=%27frozen%27"
+      request.open('GET', url, true);
+    request.send();
+    
+
   };
+
+
 }
